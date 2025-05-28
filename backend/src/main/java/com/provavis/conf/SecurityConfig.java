c76aa7f49f13c5f2f8b8da9e423c0a9a6e172450
Diego Delevedove de Oliveira <diego-delevedove.oliveira@serpro.gov.br>
2025-05-28 11:00:39 -0300

    resolvendo acesso block do h2

--- backend/src/main/java/com/provavis/conf/SecurityConfig.java
+++ backend/src/main/java/com/provavis/conf/SecurityConfig.java
@@ -1,30 +1,29 @@
 package com.provavis.conf;
 
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 	 
 		@Bean
 	    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-	    			httpSecurity
-	                .csrf(csrf -> csrf.disable())	                
+	    			httpSecurity	    			
+	                .csrf(csrf -> csrf.disable())
+	                .headers(h -> h.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
 	                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-	                .authorizeHttpRequests(authorize -> authorize	                		
-	                        .requestMatchers(HttpMethod.GET, "/backend").permitAll()    
-	                        .requestMatchers("/h2-console/**").permitAll()	                                                
-	                        .requestMatchers("/h2-console/login.do?jsessionid=**").permitAll()	                        
-	                        .anyRequest().authenticated())
-	                		.formLogin(formLogin -> formLogin
-	                        .loginPage("/login")
-	                        .permitAll());
+	                .authorizeHttpRequests(authorize -> authorize	                			                		
+	                        .requestMatchers(HttpMethod.GET, "/backend").permitAll()
+	                        .requestMatchers(toH2Console()).permitAll()	                          	                        
+	                        .anyRequest().authenticated());    				                		
 	    			return httpSecurity.build();
 	                
 	    }
